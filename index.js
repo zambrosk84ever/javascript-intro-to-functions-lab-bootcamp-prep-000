e48227f5322ed0e4a0d88138b74aa7629af0f84e
christopher zambroski <zambrosk84ever@gmail.com>
2018-02-17 05:34:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
-  console.log('hello')
+  return string.toLowerCase('HELLO')
 }
