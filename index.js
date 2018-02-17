d178160a4b49328fa9b67b5b5865913db025d812
christopher zambroski <zambrosk84ever@gmail.com>
2018-02-17 05:57:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function sayHiToGrandma(string) {
   }
   else if (string.toUpperCase() === string) 
   return "YES INDEED!";
-  
   else if (string === 'I love you, Grandma.')
   return "I love you, too.";
   }
